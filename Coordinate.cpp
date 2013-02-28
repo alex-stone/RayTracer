a77fcc6df14ab2a99205f32858b91866e5c65aa2
Alex Stone <alexstone@berkeley.edu>
2013-02-28 12:33:54 -0800

    Split Coordinate into header and class definitions

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -2,9 +2,24 @@
 #include <iostream>
 #include <time.h>
 #include <math.h>
+#include "Coordinate.h"
 
-class Coordinate {
-  public:
-    float x, y, z;
 
+Coordinate::Coordinate() {
+  setCoordinate(0.0f, 0.0f, 0.0f);
 }
+
+Coordinate::Coordinate(float a, float b, float c) {
+  setCoordinate(a, b, c);
+}
+
+void Coordinate::setCoordinate(float a, float b, float c) {
+  x = a;
+  y = b;
+  z = c;
+}
+
+
+
+
+
---
+++ Coordinate.h
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+class Coordinate {
+  private:
+    float x, y, z;
+
+  public:
+    Coordinate();
+    Coordinate(float a, float b, float c);
+
+    void setCoordinate(float a, float b, float c);
+    float getX() {return x; };
+    float getY() {return y; };
+    float getZ() {return z; };
+
+    // Add any other functions needed
+
+}
