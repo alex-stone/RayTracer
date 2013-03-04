d4cf22b4ff611ce9d35973f932dfaaf00ab0afa3
Alex Stone <alexstone@berkeley.edu>
2013-03-04 11:07:24 -0800

    Tests for Simple Ray Class done

---
+++ Ray.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+#include <float.h>
+#include "Ray.h"
+
+
+//****************************************************
+// Ray Class - Note, may want to use one Ray, and keep
+//		Changing its values so as to not have 
+//		too many of them
+//****************************************************
+
+Ray::Ray(Coordinate* pos, Vector* dir) {
+    position = pos->getCopy();
+    direction = dir->getCopy();
+ 
+    direction->normalize();
+    t_min = 1;
+    t_max = FLT_MAX;
+     
+}
+
+Ray::Ray(Coordinate* pos, Vector* dir, float min, float max) {
+    position = pos->getCopy();
+    direction = dir->getCopy();
+
+    direction->normalize();
+    t_min = min;
+    t_max = max;
+}
+
+int main(int argc, char* argv[]) {
+    Coordinate* pos = new Coordinate(0.3f, 0.2f, -0.4f);
+    Vector* dir = new Vector(0.9f, 2.0f, 3.0f);
+    Ray* r = new Ray(pos, dir);
+
+    // Add functions for Ray as needed.
+ 
+}
+
+
+
