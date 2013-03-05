b63132bfd90b21a8d7d1bd4b05ac585dc7d55105
Alex Stone <alexstone@berkeley.edu>
2013-03-05 03:17:47 -0800

    Ray - Added Getters

---
+++ Ray.h
@@ -0,0 +1,28 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include <iostream>
+#include "Vector.h"
+#include "Coordinate.h"
+
+//****************************************************
+// Ray Header Definition 
+//	Ray(t) = pos + t*dir
+//****************************************************
+
+class Ray {
+  private:
+    Coordinate* position;
+    Vector* direction;
+    float t_min, t_max;
+
+  public:
+    //Ray();
+    Ray(Coordinate* pos, Vector* dir);
+    Ray(Coordinate* pos, Vector* dir, float min, float max);
+    
+    Coordinate* getPosition() { return position; };
+    Vector* getDirection() { return direction; }; 
+};
+
+#endif
