e582467c4a00cf137d3c020b280905ca31491f52
Alex Stone <alexstone@berkeley.edu>
2013-03-05 02:02:34 -0800

    Intersection - Creating Intersection class

---
+++ Intersection.h
@@ -0,0 +1,9 @@
+#ifndef INTERSECTION_H
+#define INTERSECTION_H
+
+
+//****************************************************
+// Intersection Header Definition
+//****************************************************
+
+#endif
