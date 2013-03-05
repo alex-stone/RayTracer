a8b93ba06346af197acd0a317ad5031ceaf9593b
Alex Stone <alexstone@berkeley.edu>
2013-03-05 02:18:14 -0800

    Intersection Constructors and Getters

--- Intersection.h
+++ Intersection.h
@@ -1,9 +1,27 @@
 #ifndef INTERSECTION_H
 #define INTERSECTION_H
 
+#include "Shape.h"
+#include "LocalGeo.h"
 
 //****************************************************
 // Intersection Header Definition
 //****************************************************
+
+class Intersection {
+  private:
+    Shape* shape;
+    LocalGeo* localGeo;
+    float dist;
+
+  public:
+    Intersection(Shape* s, LocalGeo* geo, float tdist);
+   
+    // Getter Functions 
+    Shape* getShape() { return shape; };
+    LocalGeo* getLocalGeo() { return localGeo; };
+    float getDist() { return dist };
+
+};
 
 #endif
