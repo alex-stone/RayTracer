a0a2f38ebdb86f4c43e161dc84badbcde84659fa
Alex Stone <alexstone@berkeley.edu>
2013-03-05 04:04:23 -0800

    Removed BRDF from Sphere

--- Sphere.h
+++ Sphere.h
@@ -10,18 +10,13 @@
 
 class Sphere : public Shape {
   private:
-    BRDF* colorCoefficients;
     Coordinate* center;
     float radius;
   public:
     Sphere(Coordinate* cent, float rad);
-    Sphere(Coordinate* cent, float rad, BRDF* colors);
 
     LocalGeo* intersect(Ray* ray);
     Vector* getNormal(Coordinate* pos);
-
-    void setDefaultBRDF();
-    BRDF* getBRDF(){ return colorCoefficients; };
 
 };
 
