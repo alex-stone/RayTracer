8d28c11dd347941ccc092408982baaec01e97499
Alex Stone <alexstone@berkeley.edu>
2013-03-11 17:27:13 -0700

    Changed function name for rotating matrices to rotateMatrix

--- TransformMatrix.cpp
+++ TransformMatrix.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <math.h>
-#include "Matrix.h"
+#include "TransformMatrix.h"
 #include <Eigen/Dense>
+#include "Vector.h"
 
 //****************************************************
 // TransformMatrix Class Definition
 //****************************************************
 
 TransformMatrix::TransformMatrix() {
+
     for(int i = 0; i < 4; i++) {
 	for(int j = 0; j < 4; j++) {
 	    if(i == j) {
@@ -19,7 +21,7 @@ TransformMatrix::TransformMatrix() {
     }
 }
 
-TransformMatrix::TransformMatrix(float[4][4] matrix) {
+TransformMatrix::TransformMatrix(float** matrix) {
     for(int i = 0; i < 4; i++) {
 	for(int j = 0; j < 4; j++) {
 	    mat(i,j) = matrix[i][j];
@@ -32,13 +34,26 @@ TransformMatrix::TransformMatrix(Matrix4f matrix) {
 }
 
 void TransformMatrix::setValue(int row, int col, float value) {
-//    mat[row][col] = value;
      mat(row, col) = value;
 
 }
 
+TransformMatrix* TransformMatrix::inverse() {
+    TransformMatrix* trans = new TransformMatrix(mat.inverse());
+    return trans;
+}
+
+// Right Multiply
+void TransformMatrix::multMatrix(TransformMatrix* matrix) {
+     this->mat = this->mat * matrix->getMatrix();
+}
+// Left Multiply
+void TransformMatrix::multInverseMatrix(TransformMatrix* matrix) {
+    this->mat = matrix->getMatrix() * this->mat;
+}
+
 TransformMatrix* TransformMatrix::translationMatrix(float x, float y, float z) {
-     Matrix* matrix = new Matrix();
+     TransformMatrix* matrix = new TransformMatrix();
 
      matrix->setValue(0, 3, x);
      matrix->setValue(1, 3, y);
@@ -48,7 +63,7 @@ TransformMatrix* TransformMatrix::translationMatrix(float x, float y, float z) {
 }
 
 TransformMatrix* TransformMatrix::scaleMatrix(float x, float y, float z) {
-    Matrix* matrix = new Matrix();
+    TransformMatrix* matrix = new TransformMatrix();
 
     matrix->setValue(0, 0, x);
     matrix->setValue(1, 1, x);
@@ -59,8 +74,8 @@ TransformMatrix* TransformMatrix::scaleMatrix(float x, float y, float z) {
 
 
 
-TransformMatrix* TransformMatrix::rotationMatrix(float x, float y, float z, float angle) {
-    Matrix* matrix = new Matrix();
+TransformMatrix* TransformMatrix::rotateMatrix(float x, float y, float z, float angle) {
+    TransformMatrix* matrix = new TransformMatrix();
     Vector* test = new Vector(x, y , z);
     test->normalize();
 
@@ -68,25 +83,30 @@ TransformMatrix* TransformMatrix::rotationMatrix(float x, float y, float z, floa
     float v = test->getY();
     float w = test->getZ();
 
-/*
+    float a = 0.0f;
+    float b = 0.0f;
+    float c = 0.0f;
+
+    angle = angle*3.141592654/180;
+
+    // Just Rotating about a vector, that goes thru origin so (a, b, c) = (0,0,0)
     // Check it correct angle for phef
-    mat[0][0] = pow(u, 2) + (pow(v, 2) + pow(w, 2))*cos(angle);
-    mat[0][1] = u*v(1 - cos(angle))-w*sin(angle);
-    mat[0][2] = u*w(1 - cos(angle))+ v*sin(angle)l 
-    mat[0][3] = (a(v*v + w*w) -  
-    mat[1][0] =
-    mat[1][1] = pow(u, 2) + (pow(v, 2) + pow(w, 2))*cos(angle)
-    mat[1][2] = 
-    mat[1][3] = 
-    mat[2][0] = 
-    mat[2][1] =
-    mat[2][2] -
-    mat[2][3=
-    mat[3][0] = 
-    mat[3][1] = 
-    mat[3][2];
-    mat[3][3]'
-*/
+    mat(0,0) = pow(u, 2) + (pow(v, 2) + pow(w, 2))*cos(angle);
+    mat(0,1) = u*v*(1 - cos(angle))-w*sin(angle);
+    mat(0,2) = u*w*(1 - cos(angle))+ v*sin(angle);
+    mat(0,3) = (a*(v*v + w*w) - u*(b*v + c*w))*(1 - cos(angle)) + (b*w-c*v)*sin(angle);
+    mat(1,0) = u*v*(1 - cos(angle)) + w * sin(angle);
+    mat(1,1) = pow(v, 2) + (pow(u, 2) + pow(w, 2))*cos(angle);
+    mat(1,2) = v*w*(1 - cos(angle)) - u * sin(angle); 
+    mat(1,3) = (b*(pow(u,2) + pow(w,2)) - v*(a*u + c*w))*(1 - cos(angle)) + (c*u - a*w)*sin(angle);
+    mat(2,0) = u*w*(1-cos(angle)) - v*sin(angle);
+    mat(2,1) = v*w*(1-cos(angle)) + u*sin(angle);
+    mat(2,2) = pow(w,2) + (pow(u,2) + pow(v,2))*cos(angle);
+    mat(2,3) = (c*(pow(u,2) + pow(w,2)) - w*(a*u + b*v)) * (1 - cos(angle)) + (a*v - b*u)*sin(angle);
+    mat(3,0) = 0;
+    mat(3,1) = 0;
+    mat(3,2) = 0;
+    mat(3,3) = 1;
 
 
 }
@@ -94,8 +114,33 @@ TransformMatrix* TransformMatrix::rotationMatrix(float x, float y, float z, floa
 void TransformMatrix::print() {
     std::cout << mat << std::endl;
 }
-
+/*
 int main(int argc, char* argv[]) {
-	Matrix* tempMat = new Matrix();
-	tempMat->print();	
-}
+	TransformMatrix* tempMat = new TransformMatrix();
+	tempMat->print();
+
+	TransformMatrix* translateMat = tempMat->translationMatrix(0.0f, 0.0f, 2.0f);	
+	translateMat->print();
+
+
+        TransformMatrix* scaleMat = tempMat->scaleMatrix(3.0f, 0.0f, 2.0f);
+        scaleMat->print();
+
+        TransformMatrix* rotateMat = tempMat->rotationMatrix(0.0f, 0.0f, 1.0f, 90.0f);
+        rotateMat->print();
+
+	TransformMatrix* matrix = new TransformMatrix();
+
+        matrix->setValue(0,0,1.0f);
+        matrix->setValue(0,1,2.0f);
+        matrix->setValue(0,2,3.0f);
+	matrix->setValue(1,0,0.0f);
+	matrix->setValue(1,1,1.0f);
+	matrix->setValue(1,2,4.0f);
+	matrix->setValue(2,0,5.0f);
+	matrix->setValue(2,1,6.0f);
+	matrix->setValue(2,2,0.0f);
+
+ 	matrix->print();
+	matrix->inverse()->print();
+}*/
--- TransformMatrix.h
+++ TransformMatrix.h
@@ -20,11 +20,20 @@ class TransformMatrix {
 
     TransformMatrix* translationMatrix(float x, float y, float z);
     TransformMatrix* scaleMatrix(float x, float y, float z);
-    TransformMatrix* rotationMatrix(float x, float y, float z, float angle);
+    TransformMatrix* rotateMatrix(float x, float y, float z, float angle);
+
+    Eigen::Matrix<float,4,4> getMatrix() {return mat; };
+
+    TransformMatrix* inverse();
+
+    // multMatrix - Right Multiply
+    void multMatrix(TransformMatrix* matrix);
+    // multInverseMatrix - Left Multiply
+    void multInverseMatrix(TransformMatrix* matrix);
 
     void setValue(int row, int col, float value);
 
-
+    void print();
 };
 
 
